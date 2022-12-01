7a75bd690737f760a4b5a008874d63911b56f589
Jason Conway <jpc@jasonconway.dev>
2022-11-30 20:52:21 -0600

    Pedantically reference FIPS spec
    Unroll for close to double performance

--- sha256.h
+++ sha256.h
@@ -2,8 +2,8 @@
  * @file sha256.h
  * @author Jason Conway (jpc@jasonconway.dev)
  * @brief Exciting new implementation of SHA-256
- * @ref https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf
- * @version 0.9.2
+ * @ref https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf
+ * @version 2022.11
  * @date 2021-12-15
  *
  * @copyright Copyright (c) 2021-2022 Jason Conway. All rights reserved.
@@ -15,11 +15,10 @@
 #include <stdint.h>
 #include <string.h>
 
-typedef struct sha256_t
-{
+typedef struct sha256_t {
 	uint8_t data[64];
-	uint32_t data_length;
-	uint64_t bit_length;
+	uint64_t block_bytes;
+	uint64_t bits_total;
 	uint32_t state[8];
 } sha256_t;
 
@@ -34,30 +33,15 @@ void sha256_init(sha256_t *ctx);
  * @brief Append data to hash
  * 
  * @param[inout] ctx sha256_t context
- * @param[in] data data to be processed into this hash context
+ * @param[in] src data to be processed into this hash context
  * @param[in] len number of bytes to process
  */
-void sha256_append(sha256_t *ctx, const uint8_t *data, size_t len);
+void sha256_append(sha256_t *ctx, const void *src, size_t len);
 
 /**
  * @brief Finish computation and output hash
  * 
  * @param[inout] ctx sha256_t context
- * @param[out] hash destination
+ * @param[out] dst destination
  */
-void sha256_finish(sha256_t *ctx, uint8_t *hash);
-
-/**
- * @brief One-shot hash of 32-byte key
- * 
- * @param[in] key source
- * @param[out] hash destination
- */
-void sha256_digest(const uint8_t *key, uint8_t *hash);
-
-/**
- * @brief In-place variant of sha256_digest()
- * 
- * @param[inout] digest 32-byte key to hash, contents are overwritten
- */
-void sha256_self_digest(uint8_t *digest);
+void sha256_finish(sha256_t *ctx, void *dst);
